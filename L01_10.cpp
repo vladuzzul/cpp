fd75f8240bb9c4d05e5385d922486d4af75d2a6e
vladuzzul <98985996+vladuzzul@users.noreply.github.com>
2022-02-04 19:36:09 +0200

    Șir cu 2 nr impare
    Un program în care introduci prima și ultima cifra și iti arata nr. de nr. impare care se afla în acel șir(între cele 2 nr.)

---
+++ L01_10.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, nr = 0, ctrl, i;
+
+int main()
+{
+et: cout << "primul nr= "; cin >> a;
+if (a > 10000) goto et;
+cout << "ultimul numar= "; cin >> b;
+if (b > 10000) goto et;
+
+if (a > b) goto et;
+
+for (ctrl = a; ctrl <= b; ctrl++)
+{
+	nr++;
+	if (ctrl % 2 == 0) nr = nr-1;
+}
+
+cout << "Intre " << a << " si " << b << " avem " << nr << " numere impare." << endl;
+
+	return 0;
+}
